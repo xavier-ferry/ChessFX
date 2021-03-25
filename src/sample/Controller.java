bc673d8a3d7ddc2289d7ca85ab3eeb73211db8cf
xavier-ferry <xavier.ferry45@gmail.com>
2021-03-25 16:22:35 +0100

    Nouvel affichage de l'échiquier. Grossier mais OK. Prochaine étape : MAJ de l'échiquier lorsqu'on valide un coup.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -23,7 +23,7 @@ public class Controller {
 
     public void initialize() {
         this.maPartie = new Partie();
-        myTest();
+        createPlateuDeJeu();
 
     }
     // MERCI : https://stackoverflow.com/questions/37619867/how-to-display-gridpane-object-grid-lines-permanently-and-without-using-the-set/40408598
@@ -36,17 +36,15 @@ public class Controller {
             Image img = new Image("file:///Users/xavierwork/IdeaProjects/ChessFX/img/" + nomFile + ".png");
 
             ImageView view = new ImageView(img);
-            view.setFitHeight(60);
+            view.setFitHeight(50);
             view.setPreserveRatio(true);
             cell.getChildren().add(view);
         }
         cell.getStyleClass().add("cell");
-        cell.setStyle("-fx-border-style: solid inside;" +
-                "-fx-border-width: 2;");
         return cell;
     }
 
-    public void myTest(){
+    public void createPlateuDeJeu(){
         for (int x = 0 ; x < 8 ; x++) {
             ColumnConstraints cc = new ColumnConstraints();
             cc.setFillWidth(true);
@@ -64,51 +62,21 @@ public class Controller {
         for (int x = 1 ; x <= 8 ; x++) {
             for (int y = 0 ; y < 8 ; y++) {
                 String s = (char)((int)'a' +x-1) + String.valueOf(y+1);
-                gridPane.add(createCell(s), x, 8-y);
+                StackPane newCell =createCell(s);
+                if ((y % 2 == 0 && x % 2 == 1) || (y%2 == 1 && x %2 == 0)){
+                    newCell.getStyleClass().add("cell-noir");
+                } else {
+                    newCell.getStyleClass().add("cell-blanche");
+                }
+                gridPane.add(newCell, x, 8-y);
             }
         }
 
         gridPane.getStyleClass().add("grid");
     }
 
-    public void createGridPanel(){
-        for (int i = 1; i <= 8 ; i++){
-            for (int j = 0 ; j < 8 ; j++){
-                String s = (char)((int)'a' +i-1) + String.valueOf(j+1);
-                if (maPartie.getPlateau().getPiecebyNomCase(s) != null ) {
-                    String nomFile = maPartie.getPlateau().getPiecebyNomCase(s).toString();
-                    Image img = new Image("file:///Users/xavierwork/IdeaProjects/ChessFX/img/" + nomFile + ".png");
-
-                    ImageView view = new ImageView(img);
-                    view.setFitHeight(60);
-                    view.setPreserveRatio(true);
-                    gridPane.add(view, i, 8-j);
-                    gridPane.getStyleClass().add("grid-cell");
-                } else {
-                    String nom = String.valueOf(i)+"-"+String.valueOf(j+1);
-                    Node node = new Label(nom);
-                    gridPane.add(node,i,8-j);
-                    gridPane.getStyleClass().add("grid-cell-vide");
-                }
-                /*
-                Button button = new Button();
-                //Setting the size of the button
-                button.setPrefSize(60, 60);
-
-                button.setGraphic(view);
-                gridPane.add(button,i,j+1);
-                 */
-
-            }
-        }
-    }
-    public void updateGridPanelGreed(){
-        gridPane.getChildren().removeAll();
-        
-    }
-
     public void updateGridPanel(String caseDepart, String caseDestination){
-        Node depart = null ;
+  /*      Node depart = null ;
         for (Node node : gridPane.getChildren()) {
             if (GridPane.getColumnIndex(node) == 2 && GridPane.getRowIndex(node) == 2) {
                 depart = node;
@@ -118,14 +86,14 @@ public class Controller {
             }
             System.out.println(node.toString());
         }
-        gridPane.getChildren().remove(2,2);
+        gridPane.getChildren().remove(2,2);*/
+        gridPane.getChildren().set(10,createCell("e4"));
             //do something
     }
     public void printHelloWorld(){
         System.out.println(coupAJouer.getText());
         maPartie.getPlateau().deplacerPiece("e2","e4");
-        //updateGridPanel("e2","e4");
-        updateGridPanelGreed();
+        updateGridPanel("e2","e4");
     }
 
 }
